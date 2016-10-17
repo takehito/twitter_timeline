60a2c21bb3ce03bd957e392465320fdbd6627c65
takehito <utisama.produce@gmail.com>
2016-10-17 15:01:47 +0900

    ツイート情報の追加、ツイートの表示される順番を逆順に、エラーチェックを関数にまとめるという修正をしました。

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/dghubble/oauth1"
 )
@@ -17,9 +18,16 @@ type UserInfo struct {
 type Tweet struct {
 	User UserInfo `json:"user"`
 	Text string   `json:"text"`
+	When string   `json:"created_at"`
 }
 
 type tweets []Tweet
+
+func errCheck(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	count := flag.Int("count", 50, "表示したいツイート数を入力して下さい")
@@ -31,18 +39,31 @@ func main() {
 
 	path := fmt.Sprintf("https://api.twitter.com/1.1/statuses/home_timeline.json?count=%d", *count)
 	resp, err := httpClient.Get(path)
-	if err != nil {
-		log.Fatal(err)
-	}
+	errCheck(err)
 	defer resp.Body.Close()
 
 	var tweets tweets
 	reader := resp.Body
-	if err := json.NewDecoder(reader).Decode(&tweets); err != nil {
-		log.Fatal(err)
-	}
+	err = json.NewDecoder(reader).Decode(&tweets)
+	errCheck(err)
 
-	for _, tweet := range tweets {
-		fmt.Printf("\x1b[33m%s\x1b[0m: %s\n\n", tweet.User.Name, tweet.Text)
+	for i := len(tweets); i > 0; i-- {
+		tweet := tweets[i-1]
+		when, err := time.Parse(time.RubyDate, tweet.When)
+		errCheck(err)
+
+		before := time.Since(when)
+		var fmtBefore string
+		switch {
+		case before.Hours() >= 1:
+			fmtBefore = fmt.Sprintf("%d時間前", int(before.Hours()))
+		case before.Minutes() >= 1:
+			fmtBefore = fmt.Sprintf("%d分前", int(before.Minutes()))
+		default:
+			fmtBefore = fmt.Sprintf("%d秒前", int(before.Seconds()))
+		}
+
+		fmt.Printf("\x1b[33m%s\x1b[0m: %s\n", tweet.User.Name, fmtBefore)
+		fmt.Printf("%s\n\n", tweet.Text)
 	}
 }
